fa8c73fc45a3139633a05757ebaf646217fe2d5e
Hmilner1 <72082549+Hmilner1@users.noreply.github.com>
2023-03-04 06:35:09 +0000

    .

--- Mobile Test App/Assets/Scripts/Song.cs
+++ Mobile Test App/Assets/Scripts/Song.cs
@@ -6,19 +6,43 @@ using TMPro;
 
 public class Song : MonoBehaviour
 {
-    public SongSO m_CurrentSong;
-    //private TMP_Text m_SongName;
+    [SerializeField]
+    private SongSO[] m_SongList;
+    [SerializeField]
     private Text m_SongName;
+
+    private SongSO m_CurrentSong;
+    //private TMP_Text m_SongName;
     private SelectedSong m_SelectedSong;
+    int m_SelectedIndex;
     private void Awake()
     {
-        m_SongName = gameObject.GetComponentInChildren<Text>();
+        m_SelectedIndex= 0;
+        m_CurrentSong = m_SongList[m_SelectedIndex];
         m_SongName.text = m_CurrentSong.SongName;
         m_SelectedSong = GameObject.Find("SongManger").GetComponent<SelectedSong>();
     }
 
-    public void OnButtonClicked()
+    private void Update()
     {
+        m_SongName.text = m_CurrentSong.SongName;
+        m_CurrentSong = m_SongList[m_SelectedIndex];
         m_SelectedSong.m_SongSO = m_CurrentSong;
+    }
+
+    public void OnClickForward()
+    {
+        if (m_SelectedIndex < m_SongList.Length -1)
+        {
+            m_SelectedIndex++;
+        }
+    }
+
+    public void OnClickBack()
+    {
+        if (m_SelectedIndex > 0)
+        {
+            m_SelectedIndex--;
+        }
     }
 }
